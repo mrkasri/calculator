b77d6e0f69a28fe1d8f62aa64c58bb2ffa499549
Mohammed KASRI <mohammed.kasri1@gmail.com>
2021-10-23 13:23:33 +0100

    final release

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 const cWhite = Colors.white;
-const cBlack = Colors.white;
+const cBlack = Colors.black;
 const cBlue = Colors.blue;
+const cRed = Colors.redAccent;
 const cTransparent = Colors.transparent;
 
 class Calculator extends StatefulWidget {
@@ -17,10 +18,12 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   String valueText = '0';
+  String operation = '';
+
   double numOne = 0;
   double numTwo = 0;
   double result = 0;
-  double finalResult = 0;
+  double previousResult = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -66,9 +69,9 @@ class _CalculatorState extends State<Calculator> {
             flex: 1,
             child: Row(
               children: [
-                drawButton(1, 'C', cBlack, cTransparent),
+                drawButton(1, 'C', cRed, cTransparent),
                 drawButton(1, '+/-', cBlack, cTransparent),
-                drawButton(1, '%', cBlack, cTransparent),
+                drawButton(1, '⌫', cRed, cTransparent),
                 drawButton(1, '/', cBlue, cTransparent),
               ],
             ),
@@ -126,8 +129,65 @@ class _CalculatorState extends State<Calculator> {
     return Expanded(
       flex: flex,
       child: InkWell(
-        onTap: () {},
-        overlayColor: MaterialStateProperty.all(Colors.white),
+        onTap: () {
+          if (text.contains(RegExp(r'[0-9.]'))) {
+            valueText == '0' && text != '.'
+                ? valueText = text
+                : valueText.contains('.') && text == '.'
+                    ? valueText = valueText
+                    : valueText = valueText + text;
+          }
+          switch (text) {
+            case 'C':
+              {
+                valueText = '0';
+                operation = '';
+
+                numOne = 0;
+                numTwo = 0;
+                result = 0;
+                previousResult = 0;
+              }
+              break;
+            case '+/-':
+              {
+                if (valueText.startsWith('-')) {
+                  valueText = valueText.substring(1);
+                } else {
+                  valueText == '0'
+                      ? valueText = '-'
+                      : valueText = '-' + valueText;
+                }
+              }
+              break;
+            case '⌫':
+              {
+                valueText.length == 1
+                    ? valueText = '0'
+                    : valueText = valueText.substring(0, valueText.length - 1);
+              }
+
+              break;
+            case '+':
+              calculate('+');
+              break;
+            case '-':
+              calculate('-');
+              break;
+            case 'x':
+              calculate('*');
+              break;
+            case '/':
+              calculate('/');
+              break;
+            case '=':
+              calculate('=');
+              break;
+            default:
+          }
+          setState(() {});
+        },
+        highlightColor: Colors.blue,
         child: Container(
             color: containerColor,
             child: Center(
@@ -138,5 +198,45 @@ class _CalculatorState extends State<Calculator> {
             ))),
       ),
     );
+  }
+
+  void calculate(String operand) {
+    if (operation == '') {
+      previousResult = double.parse(valueText);
+    } else {
+      switch (operation) {
+        case ('+'):
+          previousResult = previousResult + double.parse(valueText);
+          break;
+        case ('-'):
+          previousResult = previousResult - double.parse(valueText);
+          break;
+        case ('*'):
+          previousResult = previousResult * double.parse(valueText);
+          break;
+        case ('/'):
+          {
+            if (valueText != '0') {
+              previousResult = previousResult / double.parse(valueText);
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                duration: Duration(seconds: 2),
+                content: Text('Can\'t divide by 0'),
+              ));
+              return;
+              //should exit here
+            }
+          }
+
+          break;
+      }
+    }
+    if (operand == '=' && valueText != '0') {
+      operation = '';
+      valueText = previousResult.toString();
+    } else {
+      operation = operand;
+      valueText = '0';
+    }
   }
 }
